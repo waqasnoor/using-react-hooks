47902faee30edf473280e393d40ecb311a98d55c
Waqas Noor <waqas2c@yahoo.com>
2020-06-28 00:53:07 +0500

    Adding Use State

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import * as R from "ramda";
 
-const InputElement = () => <input placeholder="some placeholder text" />;
+const getTargetValue = R.path(["target", "value"]);
+const InputElement = () => {
+  const [inputText, setInputText] = useState("");
+  const [inputHistory, setInputHistory] = useState([]);
+  const updateInputAndHistory = (s) => {
+    setInputText(s);
+    setInputHistory([...inputHistory, s]);
+  };
+  const getTargetValueAndSetInputText = R.pipe(
+    getTargetValue,
+    updateInputAndHistory
+  );
+  return (
+    <div>
+      <input
+        placeholder="some placeholder text"
+        onChange={getTargetValueAndSetInputText}
+      />
+      <br />
+      Input Text : {inputText}
+      <h4>History</h4>
+      {inputHistory.map((s) => (
+        <div key={s}>{s}</div>
+      ))}
+    </div>
+  );
+};
 
 export default InputElement;
